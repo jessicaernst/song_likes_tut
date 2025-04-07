f387c2ac97fac5ad1270e4b2ab709e2ebb5f7b7a
Belladonnixi <jessica_ernst@outlook.de>
2025-04-07 13:32:30 +0200

    fix: add const in main

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,6 @@ void main() {
     // ChangeNotifierProvider stellt eine Instanz von LikeModel für die gesamte App zur Verfügung.
     // create: (_) => LikeModel() erstellt eine neue Instanz von LikeModel.
     // child: App() ist die App, die mit dem LikeModel versorgt wird.
-    ChangeNotifierProvider(create: (_) => LikeModel(), child: App()),
+    ChangeNotifierProvider(create: (_) => LikeModel(), child: const App()),
   );
 }
