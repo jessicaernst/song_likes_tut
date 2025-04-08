559243fbb0acf7ef19807518ed7891d3d54b841d
Belladonnixi <jessica_ernst@outlook.de>
2025-04-08 10:25:38 +0200

    refactor: comments

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ void main() {
     // ChangeNotifierProvider stellt eine Instanz von LikeModel für die gesamte App zur Verfügung.
     // create: (_) => LikeModel() erstellt eine neue Instanz von LikeModel.
     // child: App() ist die App, die mit dem LikeModel versorgt wird.
+    // Der BuildContext muss hier nicht mitgegeben werden und kann mit dem Platzhalter _ ersetzt werden
+    // wenn die App größer werden würde und man die likes an vielen Stellen brauchen würde dann würde man da den Context reingeben
     ChangeNotifierProvider(create: (_) => LikeModel(), child: const App()),
   );
 }
