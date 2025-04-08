6b267af94fba077ed9c0949a7e2e59cf2f2c2a29
Belladonnixi <jessica_ernst@outlook.de>
2025-04-08 10:51:48 +0200

    refactor: renaming in SongListPage

--- lib/features/song_likes/screens/song_list_page.dart
+++ lib/features/song_likes/screens/song_list_page.dart
@@ -19,7 +19,7 @@ class SongListPage extends StatelessWidget {
     // Hier holen wir uns die Instanz von LikeModel, die wir in main.dart erstellt haben.
     // Provider.of<LikeModel>(context) sucht im Widget-Baum nach einem LikeModel und gibt es zurück.
     // Dies ermöglicht es uns, auf die Daten und Methoden von LikeModel zuzugreifen.
-    final likeModel = Provider.of<LikeNotifier>(context);
+    final likeNotifier = Provider.of<LikeNotifier>(context);
 
     return Scaffold(
       appBar: AppBar(title: const Text('Songs & Likes')),
@@ -29,14 +29,14 @@ class SongListPage extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             // Hier greifen wir auf die Methode totalLikes in LikeModel zu, um die Gesamtzahl der Likes anzuzeigen.
             child: Text(
-              'Gesamt-Likes: ${likeModel.totalLikes}',
+              'Gesamt-Likes: ${likeNotifier.totalLikes}',
               style: const TextStyle(fontSize: 20),
             ),
           ),
           ElevatedButton(
             onPressed: () {
               // Ruft die Methode resetLikes() in LikeModel auf, um alle Likes zurückzusetzen.
-              likeModel.resetLikes();
+              likeNotifier.resetLikes();
             },
             child: const Text('Likes zurücksetzen'),
           ),
